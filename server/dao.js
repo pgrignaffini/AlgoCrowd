29ee5cc618dbddc4ed6c59b490d1ec7bcfbbe84f
grignaffini <grignaffinipaolo@gmail.com>
2022-06-15 15:25:23 +0200

    added getApplicationFromAppId

--- server/dao.js
+++ server/dao.js
@@ -7,17 +7,17 @@ exports.createApp = (appId, creatorAddress, name, description, imageUrl, start, 
 	return new Promise((resolve, reject) => {
 		const sql = `INSERT INTO application(appId, creatorAddress, name, description, imageUrl, start, end, goal)VALUES(?,?,?,?,?,?,?,?)`;
 		db.run(sql, [
-            appId,
-            creatorAddress,
+			appId,
+			creatorAddress,
 			name,
-            description,
-            imageUrl,
-            start,
-            end,
-            goal
+			description,
+			imageUrl,
+			start,
+			end,
+			goal
 		], function (err) {
 			if (err) {
-                console.log(err)
+				console.log(err)
 				reject(err);
 				return;
 			}
@@ -31,12 +31,12 @@ exports.fundApp = (funderAddress, appId, amount) => {
 	return new Promise((resolve, reject) => {
 		const sql = `INSERT INTO fundedApplication(funderAddress, appId, amount)VALUES(?,?,?)`;
 		db.run(sql, [
-            funderAddress,
+			funderAddress,
 			appId,
 			amount
 		], function (err) {
 			if (err) {
-                console.log(err)
+				console.log(err)
 				reject(err);
 				return;
 			}
@@ -48,13 +48,13 @@ exports.fundApp = (funderAddress, appId, amount) => {
 exports.getAllApplications = () => {
 	return new Promise((resolve, reject) => {
 		const sql = "SELECT * FROM application";
-        db.all(sql, [], function (err, rows) {
+		db.all(sql, [], function (err, rows) {
 			if (err) {
 				reject(err);
 				return;
 			}
-            resolve(rows);
-        }); 
+			resolve(rows);
+		});
 	});
 };
 
@@ -66,34 +66,47 @@ exports.getApplicationsFromCreatorAddress = (creatorAddress) => {
 				reject(err);
 				return;
 			}
-            resolve(rows);
-        }); 
+			resolve(rows);
+		});
+	});
+};
+
+exports.getApplicationFromAppId = (appId) => {
+	return new Promise((resolve, reject) => {
+		const sql = "SELECT * FROM application WHERE appId = '" + appId + "'";
+		db.all(sql, [], function (err, rows) {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(rows);
+		});
 	});
 };
 
 exports.getAllFundedAppsFromFunderAddress = (funderAddress) => {
 	return new Promise((resolve, reject) => {
 		const sql = "SELECT appId, sum(amount) FROM fundedApp WHERE funderAddress = '" + funderAddress + "' GROUP BY appId";
-        db.all(sql, [], function (err, rows) {
+		db.all(sql, [], function (err, rows) {
 			if (err) {
 				reject(err);
 				return;
 			}
-            resolve(rows);
-        }); 
+			resolve(rows);
+		});
 	});
 };
 
 exports.getFundedAppAmountFromFunderAddressAndAppId = (funderAddress, appId) => {
 	return new Promise((resolve, reject) => {
 		const sql = "SELECT appId, sum(amount) FROM fundedApp WHERE funderAddress = '" + funderAddress + "' AND appId = '" + appId + "' GROUP BY appId";
-        db.all(sql, [], function (err, rows) {
+		db.all(sql, [], function (err, rows) {
 			if (err) {
 				reject(err);
 				return;
 			}
-            resolve(rows);
-        }); 
+			resolve(rows);
+		});
 	});
 };
 
