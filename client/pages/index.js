9eae249b44217c419f082763389eacb9fba59e9e
marco <marcobarca1995@gmail.com>
2022-06-18 11:56:27 +0200

    waiting for projects added

--- client/pages/index.js
+++ client/pages/index.js
@@ -21,11 +21,15 @@ export default function Home() {
   return (
     <div className="w-full bg-white dark:bg-gray-800">
       <ProjectsHeader title="Latest projects" subtitle="The authenticity of every project is verified by our team" />
-      <div className="flex mx-auto xl:max-w-7xl lg:max-w-5xl md:max-w-3xl md:px-2 px-4 bg-white dark:bg-gray-800 pt-2">
-        <div className="grid mx-auto grid-cols-1 md:grid-cols-2 gap-12">
+      {projects.length == 0 ?
+          <ProjectsHeader className="align-center" subtitle="- Waiting for projects -" />
+        :
+        <div className="flex mx-auto xl:max-w-7xl lg:max-w-5xl md:max-w-3xl md:px-2 px-4 bg-white dark:bg-gray-800 pt-2">
+          <div className="grid mx-auto grid-cols-1 md:grid-cols-2 gap-12">
             <DisplayProjects projects={projects} />
+          </div>
         </div>
-      </div>
+      }
     </div>
   )
 }
